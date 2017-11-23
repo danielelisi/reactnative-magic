34bae885d1dcc2c2379b036847596c5dd7a99382
pijrodriguez <pijrodriguez@yahoo.com.ph>
2017-11-23 15:36:09 -0800

    This adds the pie chart feature

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 
-export default class App extends React.Component {
+import AreaSpline from './js/charts/AreaSpline';
+import Pie from './js/charts/Pie';
+import Theme from './js/theme';
+import data from './resources/data';
+
+type State = {
+  activeIndex: number,
+  spendingsPerYear: any
+}
+
+export default class App extends Component {
+
+  state: State;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+      spendingsPerYear: data.spendingsPerYear,
+    };
+    this._onPieItemSelected = this._onPieItemSelected.bind(this);
+    this._shuffle = this._shuffle.bind(this);
+  }
+
+  _onPieItemSelected(newIndex){
+    this.setState({...this.state, activeIndex: newIndex, spendingsPerYear: this._shuffle(data.spendingsPerYear)});
+  }
+
+  _shuffle(a) {
+      for (let i = a.length; i; i--) {
+          let j = Math.floor(Math.random() * i);
+          [a[i - 1], a[j]] = [a[j], a[i - 1]];
+      }
+      return a;
+  }
+
   render() {
+    const height = 800;
+    const width = 500;
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container} >
+          <Text style={styles.chart_title}>Cryptocurrency</Text>
+          <Pie
+            pieWidth={380}
+            pieHeight={380}
+            onItemSelected={this._onPieItemSelected}
+            colors={Theme.colors}
+            width={width}
+            height={height}
+            data={data.spendingsLastMonth} />
+        </View>
+      </ScrollView>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'whitesmoke',
+    marginTop: 21,
   },
-});
+  chart_title : {
+    paddingTop: 15,
+    textAlign: 'center',
+    paddingBottom: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    backgroundColor:'white',
+    color: 'grey',
+    fontWeight:'bold',
+  }
+}
